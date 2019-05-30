4c9b86f217e53e8fc7670b584395fb48b37baf50
Alisher <alisher147741@gmail.com>
2019-05-31 01:11:45 +0600

    widget 1 and fix code

--- main.js
+++ main.js
@@ -1,4 +1,79 @@
-var objects = [
+//widget 1
+const widget1 = document.getElementById('widget1');
+let objectsW1 = [
+  {'title': 'ONPZ',  'firstValue': '3426', 'secondValue': '-991'},
+  {'title': 'MNPZ',  'firstValue': '1147', 'secondValue': '464'},
+  {'title': 'YANOS', 'firstValue':  '988', 'secondValue': '272'}
+];
+
+objectsW1.forEach(obj => {
+  let objectD = document.createElement('div');
+  objectD.className = 'objects__W1';
+  let objectValue = document.createElement('div');
+  objectValue.className = 'object__value';
+  let zeroLine = document.createElement('div');
+  zeroLine.className = 'zero__line';
+  let firstValue = document.createElement('div');
+  firstValue.className = 'first__value';
+  let secondValue = document.createElement('div');
+  secondValue.className = 'second__value';
+  let objectName = document.createElement('div');
+  objectName.className = 'object__name';
+
+  if (obj.title == 'ONPZ') {
+    objectValue.style.background = '-webkit-linear-gradient(90deg, #1f324a, #1a1e2b)';
+    firstValue.style.background = '#2279d8';
+    secondValue.style.background = 'rgba(34, 121, 216, .5)';
+  }
+  if (obj.title == 'MNPZ') {
+    objectValue.style.background = '-webkit-linear-gradient(90deg, #1a3943, #1a1e2b)';
+    firstValue.style.background = '#03a6a6';
+    secondValue.style.background = 'rgba(3, 166, 166, .5)';
+  }
+  if (obj.title == 'YANOS') {
+    objectValue.style.background = '-webkit-linear-gradient(90deg, #4b3077, #1a1e2b)';
+    firstValue.style.background = '#763fc1';
+    secondValue.style.background = 'rgba(118, 63, 193, .5)';
+  }
+
+  if (obj.firstValue > 0) {
+    firstValue.style.top = 191 - obj.firstValue/25 +'px';
+    firstValue.style.height = obj.firstValue/25 +'px';
+  }
+  if (obj.secondValue > 0) {
+    secondValue.style.top = 191 - obj.firstValue/25 - obj.secondValue/25 + 'px';
+    secondValue.style.height = obj.secondValue/25 + 'px';
+  }
+  if (obj.secondValue < 0) {
+    secondValue.style.top = 191 + 'px';
+    secondValue.style.height = obj.secondValue/25 * (-1) + 'px';
+  }
+
+  firstValue.innerHTML = obj.firstValue;
+  secondValue.innerHTML = obj.secondValue;
+  objectName.innerHTML = obj.title;
+
+  if (obj.title == 'ONPZ') {
+    objectName.style.borderTop = '4px solid #2279d8';
+  }
+  if (obj.title == 'MNPZ') {
+    objectName.style.borderTop = '4px solid #03a6a6';
+  }
+  if (obj.title == 'YANOS') {
+    objectName.style.borderTop = '4px solid #763fc1';
+  }
+
+  widget1.appendChild(objectD);
+  objectD.appendChild(objectValue);
+  objectD.appendChild(objectName);
+  objectD.appendChild(zeroLine);
+  objectValue.appendChild(firstValue);
+  objectValue.appendChild(secondValue);
+})
+//widget 1 end;
+
+// widget 2
+let objectsW2 = [
   ['50%', 73, 0, 16, 0, 68, 5],
   ['22%', 32, 0,  4, 0, 25, 7],
   ['28%', 41, 0,  6, 0, 41, 0]
@@ -8,17 +83,19 @@ var objects = [
 
 for (var i = 1; i < 4; i++) {
   for (var j = 0; j < 7; j++) {
-    table.rows[i].cells[j].innerHTML = objects[i-1][j];
+    table.rows[i].cells[j].innerHTML = objectsW2[i-1][j];
   }
 }
 
 for (var i = 1; i < 7; i++){
-  sum[i] = objects[0][i] + objects[1][i] + objects[2][i];
+  sum[i] = objectsW2[0][i] + objectsW2[1][i] + objectsW2[2][i];
   table.rows[0].cells[i].insertAdjacentHTML('beforeend', sum[i]);
 }
+// widget 2 end
 
+// widget 3
 const widget3 = document.getElementById('widget3');
-let org = [
+let objectsW3 = [
   {"title": "Shagol", "value": "100%"},
   {"title": "Taganrog", "value": "100%"},
   {"title": "Pskov", "value": "99.85%"},
@@ -28,32 +105,35 @@ let org = [
   {"title": "Novosibirsk", "value": "99.81%"}
 ];
 
-org.forEach(obj => {
+objectsW3.forEach(obj => {
   let organizations = document.createElement('div');
   organizations.className = 'organizations';
-  let name__org = document.createElement('div');
-  name__org.className = 'name__org';
-  let percent__d = document.createElement('div');
-  percent__d.className = 'percent__d';
+  let nameOrg = document.createElement('div');
+  nameOrg.className = 'name__org';
+  let percentD = document.createElement('div');
+  percentD.className = 'percent__d';
   let percent = document.createElement('div');
   percent.className = 'percent';
-  let percent__n = document.createElement('div');
-  percent__n.className = 'percent__n';
+  let percentN = document.createElement('div');
+  percentN.className = 'percent__n';
 
-  name__org.innerHTML = obj.title;
-  percent__n.innerHTML = obj.value;
+  nameOrg.innerHTML = obj.title;
+  percentN.innerHTML = obj.value;
   percent.style.width = obj.value;
 
   widget3.appendChild(organizations);
-  organizations.appendChild(name__org);
-  organizations.appendChild(percent__d);
-  organizations.appendChild(percent__n);
-  percent__d.appendChild(percent);
+  organizations.appendChild(nameOrg);
+  organizations.appendChild(percentD);
+  organizations.appendChild(percentN);
+  percentD.appendChild(percent);
 
 })
+// widget 3 end
+
+// widget 4
 
 const widget4 = document.getElementById('widget4');
-let events__d = [
+let eventsD = [
   {"title": "ONPZ", "value": "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci molestiae et eum aliquam beatae aliquid laudantium iste vero neque delectus fugit ullam perferendis explicabo accusantium.</p>", "date": "19.05.2019"},
   {"title": "ONPZ", "value": "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis doloremque veniam laudantium at, provident, maxime molestias possimus debitis inventore itaque.</p>", "date": "20.05.2019"},
   {"title": "MNPZ", "value": "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda sunt sit deserunt, obcaecati enim odio.</p>", "date": "21.05.2019"},
@@ -61,42 +141,43 @@ let events__d = [
   {"title": "YANOS", "value": "<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, sit. Est deserunt obcaecati, esse neque!</p>", "date": "23.05.2019"}
 ];
 
-events__d.forEach(obj =>{
+eventsD.forEach(obj =>{
   let events = document.createElement('div');
   events.className = 'events';
-  let events__head = document.createElement('div');
-  events__head.className = 'events__head';
-  let events__title = document.createElement('div');
-  events__title.className = 'events__title';
-  let events__logo = document.createElement('div');
-  events__logo.className = 'events__logo';
-  let events__body = document.createElement('div');
-  events__body.className = 'events__body';
-  let events__date = document.createElement('div');
-  events__date.className = 'events__date';
+  let eventsHead = document.createElement('div');
+  eventsHead.className = 'events__head';
+  let eventsTitle = document.createElement('div');
+  eventsTitle.className = 'events__title';
+  let eventsLogo = document.createElement('div');
+  eventsLogo.className = 'events__logo';
+  let eventsBody = document.createElement('div');
+  eventsBody.className = 'events__body';
+  let eventsDate = document.createElement('div');
+  eventsDate.className = 'events__date';
 
-  events__title.innerHTML = obj.title;
-  events__logo.innerHTML = '<i class="fas fa-city"></i><br>ER';
-  events__body.innerHTML = obj.value;
-  events__date.innerHTML = obj.date;
+  eventsTitle.innerHTML = obj.title;
+  eventsLogo.innerHTML = '<i class="fas fa-city"></i><br>ER';
+  eventsBody.innerHTML = obj.value;
+  eventsDate.innerHTML = obj.date;
 
   if (obj.title == 'ONPZ'){
-    events__title.style.background = '#2279d8';
-    events__head.style.borderLeft = '2px solid #2279d8';
+    eventsTitle.style.background = '#2279d8';
+    eventsHead.style.borderLeft = '2px solid #2279d8';
   }
   if (obj.title == 'MNPZ'){
-    events__title.style.background = '#03a6a6';
-    events__head.style.borderLeft = '2px solid #03a6a6';
+    eventsTitle.style.background = '#03a6a6';
+    eventsHead.style.borderLeft = '2px solid #03a6a6';
   }
   if (obj.title == 'YANOS'){
-    events__title.style.background = '#763fc1';
-    events__head.style.borderLeft = '2px solid #763fc1';
+    eventsTitle.style.background = '#763fc1';
+    eventsHead.style.borderLeft = '2px solid #763fc1';
   }
 
   widget4.appendChild(events);
-  events.appendChild(events__head);
-  events__head.appendChild(events__title);
-  events__head.appendChild(events__logo);
-  events.appendChild(events__body);
-  events.appendChild(events__date);
+  events.appendChild(eventsHead);
+  eventsHead.appendChild(eventsTitle);
+  eventsHead.appendChild(eventsLogo);
+  events.appendChild(eventsBody);
+  events.appendChild(eventsDate);
 })
+// widget 4 end
