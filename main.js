869b2c4023e7c0e1359cf1268a6936e9ae230202
Alisher <alisher147741@gmail.com>
2019-05-30 14:17:23 +0600

    widget 3

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var objects = [
   table = document.getElementById("table");
 
 
-  
+
 for (var i = 1; i < 4; i++) {
   for (var j = 0; j < 7; j++) {
     table.rows[i].cells[j].innerHTML = objects[i-1][j];
@@ -19,24 +19,37 @@ for (var i = 1; i < 7; i++){
   table.rows[0].cells[i].insertAdjacentHTML('beforeend', sum[i]);
 }
 
+const widget3 = document.getElementById('widget3');
+let org = [
+  {"title": "Shagol", "value": "100%"},
+  {"title": "Taganrog", "value": "100%"},
+  {"title": "Pskov", "value": "99.85%"},
+  {"title": "Orenburg", "value": "99.87%"},
+  {"title": "Sheremetyevo", "value": "99.62%"},
+  {"title": "Engels", "value": "97.43%"},
+  {"title": "Novosibirsk", "value": "99.81%"}
+];
 
-var name__org = [
-  ['Shagol', "100"],
-  ['Taganrog', "100"],
-  ['Pskov', "99.85"],
-  ['Orenburg', "99.87"],
-  ['Sheremetyevo', "99.62"],
-  ['Engels', "97.43"],
-  ['Novosibirsk', "99.81"]
-],
-table__2 = document.getElementById('table__2');
+org.forEach(obj => {
+  let organizations = document.createElement('div');
+  organizations.className = 'organizations';
+  let name__org = document.createElement('div');
+  name__org.className = 'name__org';
+  let percent__d = document.createElement('div');
+  percent__d.className = 'percent__d';
+  let percent = document.createElement('div');
+  percent.className = 'percent';
+  let percent__n = document.createElement('div');
+  percent__n.className = 'percent__n';
 
-for (var i = 1; i < 8; i++) {
-  var j = 0;
-  table__2.rows[i].cells[j].innerHTML = name__org[i-1][j];
-}
+  name__org.innerHTML = obj.title;
+  percent__n.innerHTML = obj.value;
+  percent.style.width = obj.value;
 
-for (var i = 1; i < 8; i++) {
-  var j = 2;
-  table__2.rows[i].cells[j].insertAdjacentHTML('afterbegin', name__org[i-1][j-1]);
-}
+  widget3.appendChild(organizations);
+  organizations.appendChild(name__org);
+  organizations.appendChild(percent__d);
+  organizations.appendChild(percent__n);
+  percent__d.appendChild(percent);
+
+})
